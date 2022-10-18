a14c45a4f6b280d8ef2e0be3fded933d7c276ad4
RobertPamparau <robert.pamparau@assist.ro>
2022-10-18 10:54:46 +0300

    First commit

---
+++ src/hello.js
@@ -0,0 +1,13 @@
+const hello = async (event) => {
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'hello world'
+            })
+        }
+};
+
+module.exports ={
+    handler:hello
+}
